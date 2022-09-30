855b1924f19dc925fbbe23010e07ef4788058aa2
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2022-09-30 15:05:14 -0700

    Start combining DL and conversion script

--- create_images.sh
+++ create_images.sh
@@ -3,15 +3,15 @@
 # Create export directory
 mkdir export_images
 
-# Instantiate filenames variable
-filenames=1
+# Instantiate export_filenames variable
+export_filenames=1
 
 # Loop through images and merge
 for input_image in ./card_images/*.png
 do
     printf "Converting $input_image...\n"
-    magick composite -geometry +1632+200 $input_image background.png export_images/$filenames.png
-    let filenames=filenames+1
+    magick composite -geometry +1632+200 $input_image background.png export_images/$export_filenames.png
+    let export_filenames=export_filenames+1
 done
 
 # Done
--- download_images.sh
+++ download_images.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# Create export directory
+# Create export directories
 mkdir card_images
+mkdir export_images
 
-# Get list of png files
+# Get list of png files, export to temp file
 python3 scry $1 --print="%{image_uris.png}" > tmp.txt
+printf "SUCCESS: Got list of card images\n"
 
-# Instantiate filename variable
-filenames=1
+# Instantiate variable for card filenames
+card_filenames=1
 
-# The loop
+# Download images of all cards
 for card_image in "${(@f)"$(<tmp.txt)"}"
 {
-  printf "Downloading $card_image - $filenames \n"
   sleep 0.1
-  wget -q -O ./card_images/$filenames.png $card_image
-  let filenames=filenames+1
+  wget -q -O ./card_images/$card_filenames.png $card_image
+  let card_filenames=card_filenames+1
+  printf "    Downloaded $card_image - $card_filenames\n"
 }
 
+# Cleanup tmp.txt and update status
 rm tmp.txt
+printf "SUCCESS: Downloaded all card images\n"
 
-printf "Finished downloading images"
+# Loop through images and merge
+for input_image in ./card_images/*
+do
+    printf "$input_image\n"
+    export_filename=$(printf "$input_image" | sed 's@./card_images/@@')
+    printf "$export_filename\n"
+    # magick composite -geometry +1632+200 $input_image background.png export_images/$export_filename.png
+    # printf "    Converted $input_image...\n"
+done
+
+# Done
+printf "SUCCESS: All thumbnails created"
