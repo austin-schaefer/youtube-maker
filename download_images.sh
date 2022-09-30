78f7e3c1319bff628afb9e06ac007dd481a8999e
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2022-09-30 15:39:23 -0700

    Merge image maker scripts

--- download_images.sh
+++ download_images.sh
@@ -14,10 +14,10 @@ card_filenames=1
 # Download images of all cards
 for card_image in "${(@f)"$(<tmp.txt)"}"
 {
-  sleep 0.1
+  sleep 0.11
   wget -q -O ./card_images/$card_filenames.png $card_image
-  let card_filenames=card_filenames+1
   printf "    Downloaded $card_image - $card_filenames\n"
+  let card_filenames=card_filenames+1
 }
 
 # Cleanup tmp.txt and update status
@@ -27,11 +27,12 @@ printf "SUCCESS: Downloaded all card images\n"
 # Loop through images and merge
 for input_image in ./card_images/*
 do
-    printf "$input_image\n"
+    # printf "    $input_image\n"
+    # Create variable to set same filename as source image
     export_filename=$(printf "$input_image" | sed 's@./card_images/@@')
-    printf "$export_filename\n"
-    # magick composite -geometry +1632+200 $input_image background.png export_images/$export_filename.png
-    # printf "    Converted $input_image...\n"
+    # printf "    $export_filename\n"
+    magick composite -geometry +1632+200 $input_image background.png export_images/$export_filename
+    printf "    Converted $input_image...\n"
 done
 
 # Done
