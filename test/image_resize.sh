e035992f7c80434f0fdf8c1f3b95fd972eba35f3
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2023-07-16 13:57:21 -0700

    Test: Working resize code but it's awk

--- test/image_resize.sh
+++ test/image_resize.sh
@@ -9,8 +9,11 @@ set -o nounset
 set -o pipefail
 
 # Create grid image
-convert too_tall.png -geometry x1400 1400_grid.png
-convert too_tall.png -geometry x1400 1400_grid.png
+# Works but can be very slow / ineffecient on tall grids in particular
+# convert too_tall.png -geometry 2500x1400^ too_tall_inter.png
+# convert too_tall_inter.png -geometry 2500x1400 too_tall_final.png
+# convert too_long.png -geometry 2500x1400^ too_long_inter.png
+# convert too_long_inter.png -geometry 2500x1400 too_long_final.png
 
 # Create composite grid image
-magick composite -gravity center 1400_grid.png resources/title_background.png images_export/grid.png
+# magick composite -gravity center 1400_grid.png resources/title_background.png images_export/grid.png
