51f73f835729e70afc26f78847e578d8862abbe7
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2023-07-16 16:41:18 -0700

    Feat: Resize art gracefully, new art background

--- cleanup.sh
+++ cleanup.sh
@@ -12,7 +12,8 @@ rm -rf images_card
 rm -rf images_art
 rm -rf images_export
 rm -rf images_resized_art
+rm -rf images_export_w_art
 rm -f temp_card_images.txt
 rm -f temp_art_images.txt
 
-printf "SUCCESS: Removed all export files"
+printf "SUCCESS: Removed all export files\n\n"
--- download_images.sh
+++ download_images.sh
@@ -23,11 +23,12 @@ mkdir images_export
 if [[ "$include_card_art" == "Y" ]] ; then
     mkdir images_art
     mkdir images_resized_art
+    mkdir images_export_w_art
 fi
 
 # Get list of card images, export to temp file
 python3 scry $scryfall_search --print="%{image_uris.png}" > temp_card_images.txt
-printf "SUCCESS: Got list of card images\n"
+printf "SUCCESS: Got list of card images\n\n"
 
 # Instantiate variable for card filenames
 card_count=1
@@ -41,16 +42,15 @@ for card_image in "${(@f)"$(<temp_card_images.txt)"}"
     printf "    Downloaded card: $card_image - $card_numbers\n"
     let card_count=card_count+1
 }
-
 # Cleanup temp_card_images.txt and update status
 rm temp_card_images.txt
-printf "SUCCESS: Downloaded all card images\n"
+printf "SUCCESS: Downloaded all card images\n\n"
 
 # Get art using same logic, if user selected
 if [[ "$include_card_art" == "Y" ]] ; then
     # Get list of art images, export to temp file
     python3 scry $scryfall_search --print="%{image_uris.art_crop}" > temp_art_images.txt
-    printf "SUCCESS: Got list of art images\n"
+    printf "SUCCESS: Got list of art images\n\n"
     
     # Instantiate variable for art filenames
     art_count=1
@@ -66,7 +66,7 @@ if [[ "$include_card_art" == "Y" ]] ; then
     }
     # Cleanup temp_art_images.txt and update status
     rm temp_art_images.txt
-    printf "SUCCESS: Downloaded all art images\n"
+    printf "SUCCESS: Downloaded all art images\n\n"
 
     # Resize all art to consistent size
     # It looks messy, but basically it just reads width and height
@@ -89,31 +89,61 @@ if [[ "$include_card_art" == "Y" ]] ; then
         fi
         printf "    Resized art $input_art...\n"
     }
+
+    printf "SUCCESS: Resized all art images\n\n"
 elif [[ "$include_card_art" == "N" ]] ; then
-    printf "NOTE: Card art is N, skipping download\n"
+    printf "NOTE: Card art is N, skipping art download\n\n"
 fi
 
-# Create export images
+# Create export images, w/ or w/o art
 if [[ "$include_card_art" == "Y" ]] ; then
-    printf "Whatevs\n"
-elif [[ "$include_card_art" == "N" ]] ; then
     # Loop through images and merge
     for input_image in ./images_card/*
     {
         # Create variable to set same filename as source image
         export_filename=$(printf "$input_image" | sed 's@./images_card/@@')
         # printf "    $export_filename\n"
-        magick composite -geometry +1632+200 $input_image resources/card_background.png images_export/$export_filename
+        magick composite -geometry +180+200 $input_image resources/card_and_art_background.png images_export/$export_filename
         printf "    Converted $input_image...\n"
     }
-    # Exports created
-    printf "SUCCESS: All export images created\n"
+    printf "SUCCESS: All export images created\n\n"
+
+    # Loop through card art images and merge
+    for input_image in ./images_resized_art/*
+    {
+        # Create variable to set same filename as source image
+        export_filename=$(printf "$input_image" | sed 's@./images_resized_art/@@')
+        # Read image height and do some math to align it properly
+        image_width=$(identify -ping -format '%w' $input_image)
+        image_height=$(identify -ping -format '%h' $input_image)
+        horizontal_offset=$(( 1120 + ( (1440 - $image_width) / 2 ) ))
+        vertical_offset=$(( (1440 - $image_height) / 2 ))
+        # Export final image
+        magick composite -geometry +$horizontal_offset+$vertical_offset $input_image images_export/$export_filename images_export_w_art/$export_filename
+        printf "    Added art to $input_image...\n"
+    }
+    
+    # Remove original export directory and rename
+    rm -rf images_export
+    mv images_export_w_art images_export
+    printf "SUCCESS: All art export images created\n\n"
+elif [[ "$include_card_art" == "N" ]] ; then
+    # Loop through card images and merge
+    for input_image in ./images_card/*
+    {
+        # Create variable to set same filename as source image
+        export_filename=$(printf "$input_image" | sed 's@./images_card/@@')
+        # Export final image
+        magick composite -geometry +1632+200 $input_image resources/card_only_background.png images_export/$export_filename
+        printf "    Converted $input_image...\n"
+    }
+    printf "SUCCESS: All export images created\n\n"
 fi
 
 # Create grid image
 cd images_card
 montage -density 200 -tile $grid_arrangement -geometry +10+40 -background none *.png grid.png
-printf "SUCCESS: Card grid created\n"
+printf "SUCCESS: Card grid created\n\n"
 
 # Resize grid image conditionally based on size
 grid_width=$(identify -ping -format '%w' grid.png)
@@ -137,4 +167,4 @@ cp grid_resized.png ..
 cd ..
 magick composite -gravity center grid_resized.png resources/title_background.png images_export/grid.png
 rm grid_resized.png
-printf "SUCCESS: Composite grid created\n"
+printf "SUCCESS: Composite grid created\n\n"
